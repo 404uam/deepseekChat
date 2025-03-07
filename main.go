d29e77baeb8229851e8304c0932659f646a9e839
Louis Mau <github@louismau.com>
2025-03-07 15:54:11 -0700

    Remove api key and other

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+
+	"github.com/openai/openai-go" // imported as openai
+	"github.com/openai/openai-go/option"
+)
+
+func main() {
+	client := openai.NewClient(
+		option.WithAPIKey("sk-829c6ac87bd64bd5816606de78289470"), // defaults to os.LookupEnv("OPENAI_API_KEY")
+		option.WithBaseURL("https://api.deepseek.com"),
+	)
+
+	chatCompletion, err := client.Chat.Completions.New(context.Background(),
+		openai.ChatCompletionNewParams{
+			Messages: openai.F([]openai.ChatCompletionMessageParamUnion{
+				openai.UserMessage("Hello say hi"),
+			}),
+			Model: openai.F(openai.ChatModelDeepSeek),
+		})
+
+	/* chatCompletion, err := client.Chat.Completions.New(context.TODO(), openai.ChatCompletionNewParams{
+		Messages: openai.F([]openai.ChatCompletionMessageParamUnion{
+			openai.UserMessage("Say this is a test"),
+		}),
+		Model: openai.F(openai.ChatModelGPT4o),
+	}) */
+	if err != nil {
+		panic(err.Error())
+	}
+	println(chatCompletion.Choices[0].Message.Content)
+
+}
